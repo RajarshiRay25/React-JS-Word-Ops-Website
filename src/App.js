77156cda5cb5fe943414f4158aad7ea2c027e1f4
RajarshiRay25 <rayrajarshi9@gmail.com>
2022-04-20 15:56:23 +0530

    Router Add

--- src/App.js
+++ src/App.js
@@ -2,32 +2,52 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
-// import About from './Components/About';
-import React,{useState} from 'react';
+import About from './Components/About';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
-  const[mode,setMode] = useState("light");          //For Button Mode
-  const[typeForm,settypeForm]=useState("Dark")      //For Button Text
-  const modeToggle =()=>{
-    if (mode === "light" && typeForm ==="Dark"){
+  const [mode, setMode] = useState("light");          //For Button Mode
+  const [typeForm, settypeForm] = useState("Dark")      //For Button Text
+  const modeToggle = () => {
+    if (mode === "light" && typeForm === "Dark") {
       setMode("dark")
       settypeForm("Light")
       document.body.style.backgroundColor = "#2C3333"
       document.body.style.color = "white"
+      document.title = "Text-Utils : Dark Mode"
     }
-    else{
+    else {
       setMode("light")
       settypeForm("Dark")
       document.body.style.backgroundColor = "white"
       document.body.style.color = "black"
+      document.title = "Text-Utils : Light Mode"
     }
   }
 
   return (
     <>
-    <Navbar text="TextUtils" mode={mode} modeToggle={modeToggle} typeForm={typeForm}/>
-    <TextForm textHead ="Enter Text"/>
-    {/* <About/> */}
+
+      <Router>
+        <Navbar text="TextUtils" mode={mode} modeToggle={modeToggle} typeForm={typeForm} />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <TextForm textHead="Enter Text" />
+          </Route>
+        </Switch>
+      </Router>
+
+
     </>
   );
 }
